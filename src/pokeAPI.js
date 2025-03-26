1a3ea523bf07373aae737a1595d4efdc938be60a
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-03-25 22:50:20 -0500

    switched API to pokeAPI
    superheroAPI does not allow for cors mode so project will now use pokeAPI

---
+++ src/pokeAPI.js
@@ -0,0 +1,21 @@
+const pokemonNames = ["charmander", "bulbasaur", "squirtle", "pikachu", "torchic", "chimchar", "mew", "jigglypuff"];
+
+export const getPokemon = async () => {
+    try {
+        const pokePromises = pokemonNames.map(async (pokemon) => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+            const data = await response.json();
+            return {
+                name: data.forms.name,
+                image: data.sprites.front_default,
+            }
+        })
+
+        const pokemon = await Promise.all(pokePromises);
+        return pokemon;
+    } 
+    catch (error) {
+        console.error("Error fetching heroes:", error);
+        return [];
+    }
+}
