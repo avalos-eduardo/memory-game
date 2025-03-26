8f1d32a23dd05df68379a44971a5df7ad6849ce4
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-03-25 23:33:53 -0500

    utilizes useState and useEffect to render created components from pokeAPI
    useEffect is used here to retrievepokemon data from the pokeAPI and work in conjuction with useState to keep track of which pokemon are to be rendered on the website. The CardsDisplay component pulls the data stored in useState and maps each pokemon to its own Card component. useEffect is also used here to ensure the API is only fetched on mount.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import { getPokemon } from "./pokeAPI";
+import Card from "./components/Card";
+import CardsDisplay from "./components/CardsDisplay";
+import Header from "./components/Header";
+import Scorekeeper from "./components/Scorekeeper";
+
 import "./styles/App.css";
+import "./styles/Header.css";
+import "./styles/Scorekeeper.css";
 
 export default function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const pokeData = await getPokemon();
+      setPokemon(pokeData);
+    };
+
+    fetchPokemon();
+  }, []);
 
   return (
     <>
-      
+      <Header title="Pokémon Memory Game" />
+      <Scorekeeper />
+      <CardsDisplay>
+        {pokemon.map((poke) => (
+          <Card pokeImg={poke.image} pokeName={poke.name} />
+        ))}
+      </CardsDisplay>
     </>
-  )
+  );
 }
