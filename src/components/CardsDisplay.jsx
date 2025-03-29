78d91c81b3a3eaa17e256baa05b6e831c0749075
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-03-28 20:46:25 -0500

    Added footer area under card components

--- src/components/CardsDisplay.jsx
+++ src/components/CardsDisplay.jsx
@@ -1,3 +1,11 @@
 export default function CardsDisplay({ children }) {
-  return <div className="cards-display">{children}</div>;
+  return (
+    <>
+      <div className="cards-display">{children}</div>
+      <p className="footer">
+        Welcome to the Pokémon Memory Game! Try not to click on the same Pokémon
+        more than once!
+      </p>
+    </>
+  );
 }
