ec68dd7c32eb7f2da95c3af88736aae47e8395ad
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-03-29 13:40:45 -0500

    formatted for consistency

--- src/components/Scorekeeper.jsx
+++ src/components/Scorekeeper.jsx
@@ -1,8 +1,8 @@
-export default function Scorekeeper({score = 0, highScore = 0}){
-    return (
-        <div className="scorekeeper">
-            <p>Score: {score}</p>
-            <p>High Score: {highScore}</p>
-        </div>
-    )
+export default function Scorekeeper({ score, highScore }) {
+  return (
+    <div className="scorekeeper">
+      <p>Score: {score}</p>
+      <p>High Score: {highScore}</p>
+    </div>
+  );
 }
