627a07d12042c90bb83260b3db1f2b67fd0d73e3
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-03-30 09:52:51 -0500

    style changes for mobile view

--- src/components/CardsDisplay.jsx
+++ src/components/CardsDisplay.jsx
@@ -1,8 +1,8 @@
 export default function CardsDisplay({ children, message }) {
   return (
-    <>
+    <div className="display-wrapper">
       <div className="cards-display">{children}</div>
       <p className="message">{message}</p>
-    </>
+    </div>
   );
 }
